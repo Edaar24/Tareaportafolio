4f6e6ed7e0201b3ccaba4862390860f9938b1b35
Edaar24 <edia_amayar38@unicah.edu>
2024-07-06 23:40:37 -0600

    first commit

---
+++ context/ProdsPage.js
@@ -0,0 +1,20 @@
+export default {
+    todos: [
+        {
+            "imgUrl": "https://placehold.co/300x200",
+            "title": "Producto 1",
+            "content": "Texto de la Producto 1"
+        },
+        {
+            "imgUrl": "https://placehold.co/300x200",
+            "title": "Producto 2",
+            "content": "Texto de la Producto 2"
+        },
+        {
+            "imgUrl": "https://placehold.co/300x200",
+            "title": "Producto 3",
+            "content": "Texto de la Producto 3"
+        },
+    
+    ]
+}
